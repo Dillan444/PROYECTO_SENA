df59ac1d27a2f50b88e5292d5e5295ee129e2a27
Dillan Rojas <rojasdylan444@gmail.com>
2022-03-24 11:52:04 -0500

    Doc: Error & IconsStyles v.12.4.2
    Corrección de un error al momento de enviar datos del formulario, en una variable que no recibía un dato tipo string.
    Se agregaron iconos a la pagina más intuitiva y una nueva función con un botón(reset), para que pueda restaurar los valores del formulario.
    Se agregaron correcciones de posicionamiento y dimensiones absolutas.

--- Web_Page_IED.E.S.M/html/actualizarMisDatos.php
+++ Web_Page_IED.E.S.M/html/actualizarMisDatos.php
@@ -6,8 +6,6 @@
     session_start();
     $usuario = $_SESSION["usuario"];
 
-    $rol = $_GET['r'];
-
     if (!isset($usuario)) {
         header("Location: ./login.html");
     }
@@ -16,6 +14,15 @@
       FROM usuario u INNER JOIN datos_adicionales da ON da.id_datos_adicionales = u.id_datos_adicionales AND u.nombre_perfil = '$usuario'";
 
     $datos = $conx -> query($sqlDatos) -> fetch_array();
+
+    
+    switch ($datos['id_rol']) {
+        case 'E': $rol = "Estudiante"; $icon = '<i class="fas fa-user-graduate"></i>'; break;
+        case 'D': $rol = "Docente"; $icon = "<i class='fas fa-chalkboard-teacher'></i>"; break;
+        case 'A': $rol = "Administrador"; $icon = '<i class="fas fa-user-cog"></i>'; break;
+
+        default: $rol = "Rol desconocido"; $icon = "!"; break;
+    }
 
     #CONSUTA DE LOS nompre_perfil YA EXISTENTES
     $sqlUsers = $conx -> query("SELECT u.nombre_perfil
@@ -40,6 +47,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://kit.fontawesome.com/96a22afc86.js" crossorigin="anonymous"></script>
     <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
     <link rel="stylesheet" href="../css/generalStyles.css">
     <link rel="stylesheet" href="../css/actualizarMisDatos.css">
@@ -47,26 +55,19 @@
     <title>Actualiza tus datos</title>
 </head>
 <body>
-    <a href="./<?php echo $rol;?>-index.php">
-        <button id="back">Regresar</button>
-    </a>
+    
     <section class="perfil-info">
         <h1>Actualiza tus datos</h1>
         <span><?= buscarNombreUsuario($usuario, $conx, $datos['id_rol']);?></span>
     </section>
     <main class="content">
+        <a href="./<?php echo $rol;?>-index.php">
+            <button id="back"> <img src="../IMG/icons/arrow-left-solid.svg" alt="Regresar"> <span>Regresar</span></button>
+        </a>
         <section class="perfil">
     
             <img src="../IMG/<?php echo ($datos['sexo'] === "F")?"userWoman":"userMan";?>.png" alt="imagen de perfil del usuario" class="perfil-imagen">
-            <span class="perfil-rol"><?php
-                switch ($datos['id_rol']) {
-                    case 'E': echo "Estudiante"; break;
-                    case 'D': echo "Docente"; break;
-                    case 'A': echo "Administrador"; break;
-    
-                    default: echo "Rol desconocido"; break;
-                }
-            ?></span>
+            <span class="perfil-rol"><?php echo $rol; ?><?php echo $icon?></span>
         </section>
         <div class="line-gradient"></div>
         <section class="perfil-data">
@@ -103,6 +104,10 @@
               </label>
             <div class="perfil-data--actualizar">
                 <input type="submit" value="Actualizar" name="accion" id="actualizar" >
+                <button type="reset" id="reset">
+                    <svg viewBox="0 0 512 512"><path d="M496 48V192c0 17.69-14.31 32-32 32H320c-17.69 0-32-14.31-32-32s14.31-32 32-32h63.39c-29.97-39.7-77.25-63.78-127.6-63.78C167.7 96.22 96 167.9 96 256s71.69 159.8 159.8 159.8c34.88 0 68.03-11.03 95.88-31.94c14.22-10.53 34.22-7.75 44.81 6.375c10.59 14.16 7.75 34.22-6.375 44.81c-39.03 29.28-85.36 44.86-134.2 44.86C132.5 479.9 32 379.4 32 256s100.5-223.9 223.9-223.9c69.15 0 134 32.47 176.1 86.12V48c0-17.69 14.31-32 32-32S496 30.31 496 48z"/>
+                    </svg>
+                </button>
             </div>
           </form>
     
